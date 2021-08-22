757b0c9d76eb6eaaff53d4a19871e04a6bd5a848
JungHyun0115 <agnes.ojh@gmail.com>
2021-08-22 15:57:17 +0900

    TVUser 클래스 TV 인터페이스 타입의 변수로 LgTV 객체 참조

--- springTest/src/main/java/polymorphism/TVUser.java
+++ springTest/src/main/java/polymorphism/TVUser.java
@@ -3,7 +3,7 @@ package polymorphism;
 public class TVUser {
 
 	public static void main(String[] args) {
-		TV tv = new SamsungTV();
+		TV tv = new LgTV();
 		tv.powerOn();
 		tv.volumeUp();
 		tv.volumeDown();
