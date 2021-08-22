c93c24011998194eecf458f98edbdf2c6cc5dec8
JungHyun0115 <agnes.ojh@gmail.com>
2021-08-22 15:55:50 +0900

    TVUser 클래스 TV 인터페이스 타입의 변수로 SamsungTV 객체 참조
    묵시적 형변환을 이용하여 객체를 참조하면 SamsungTV를 LgTV 객체로 변경할 때, 참조하는 객체만 변경하면 되므로 객체를
    쉽게 교환할 수 있음(유지보수 편리)

--- springTest/src/main/java/polymorphism/TVUser.java
+++ springTest/src/main/java/polymorphism/TVUser.java
@@ -3,11 +3,11 @@ package polymorphism;
 public class TVUser {
 
 	public static void main(String[] args) {
-		LgTV tv = new LgTV();
-		tv.turnOn();
-		tv.soundUp();
-		tv.soundDown();
-		tv.turnOff();
+		TV tv = new SamsungTV();
+		tv.powerOn();
+		tv.volumeUp();
+		tv.volumeDown();
+		tv.powerOff();
 		
 	}
 
