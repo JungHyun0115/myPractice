303b8717c528737c59aa6fae43141ef5c92b5072
JungHyun0115 <agnes.ojh@gmail.com>
2021-08-23 23:36:21 +0900

    @Component

--- springTest/src/main/java/polymorphism/LgTV.java
+++ springTest/src/main/java/polymorphism/LgTV.java
@@ -1,5 +1,8 @@
 package polymorphism;
 
+import org.springframework.stereotype.Component;
+
+@Component("tv")
 public class LgTV implements TV{
 	public void powerOn() {
 		System.out.println("LgTV---Àü¿ø ÄÒ´Ù.");
