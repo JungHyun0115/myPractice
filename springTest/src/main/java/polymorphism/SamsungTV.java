0a8a206f7ffe500ac3f6e78d6f597d3003aa5a72
JungHyun0115 <agnes.ojh@gmail.com>
2021-08-23 22:22:00 +0900

    다중 변수 매핑

--- springTest/src/main/java/polymorphism/SamsungTV.java
+++ springTest/src/main/java/polymorphism/SamsungTV.java
@@ -2,6 +2,7 @@ package polymorphism;
 
 public class SamsungTV implements TV {
 	private SonySpeaker speaker;
+	private int price;
 	
 	public void initMethod() {
 		System.out.println("객체 초기화 작업 처리..");
@@ -10,6 +11,7 @@ public class SamsungTV implements TV {
 	public void destroyMethod() {
 		System.out.println("객체 삭제 전에 처리할 로직 처리...");
 	}
+	
 
 	public SamsungTV() {
 		System.out.println("===> SamsungTV(1) 객체 생성");
@@ -19,9 +21,16 @@ public class SamsungTV implements TV {
 		System.out.println("===> SamsungTV(2) 객체 생성");
 		this.speaker = speaker;
 	}
+	
+	public SamsungTV(SonySpeaker speaker, int price) {
+		System.out.println("===> SamsunTV(3) 객체 생성");
+		this.speaker = speaker;
+		this.price = price;
+	}
+
 
 	public void powerOn() {
-		System.out.println("SamsungTV---전원 켠다.");
+		System.out.println("SamsungTV---전원 켠다. (가격 : " + price + ")");
 	}
 
 	public void powerOff() {
