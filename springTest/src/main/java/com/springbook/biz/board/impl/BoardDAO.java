78f22ba1b0e04d847a9b14ebe4c62aaa98412f0e
JungHyun0115 <agnes.ojh@gmail.com>
2021-08-25 00:34:04 +0900

    LogAdvice 클래스 구형

--- springTest/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
+++ springTest/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
@@ -21,7 +21,7 @@ public class BoardDAO {
 
 	// SQL 명령어들
 	private final String BOARD_INSERT = "insert into board2(seq, title, writer, content) "
-			+ "values((select nvl(max(seq),0)+1 from board),?,?,?)";
+			+ "values((select nvl(max(seq),0)+1 from board2),?,?,?)";
 	private final String BOARD_UPDATE = "update board2 set title=?,content=? where seq=?";
 	private final String BOARD_DELETE = "delete board2 from where seq=?";
 	private final String BOARD_GET = "select * from board2 where seq=?";
