ccbc1055c55c744f552006af9643e26c56357958
JungHyun0115 <agnes.ojh@gmail.com>
2021-08-25 22:49:43 +0900

    애스팩트 설정

--- springTest/src/main/java/com/springbook/biz/common/LogAdvice.java
+++ springTest/src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,10 +1,12 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Service;
 
 @Service
+@Aspect // Aspect = Pointcut + Advice
 public class LogAdvice {
 
 	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
